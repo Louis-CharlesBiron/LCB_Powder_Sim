f9e0845306e59d02966cc883ba738e9134c1a1c0
Louis-Charles Biron <louilresbiron6@gmail.com>
2026-02-06 11:44:20 -0500

    (See #56) Started Desktop App

--- deployements/desktop/src/assets/scripts/setup.js
+++ deployements/desktop/src/assets/scripts/setup.js
@@ -2,7 +2,7 @@ Neutralino.init()
 const EXIT_KEY = "escape"
 
 document.addEventListener("keydown", e=>{
-    if (e.key.toLowerCase()==EXIT_KEY) {
+    if (e.key.toLowerCase() === EXIT_KEY) {
         Neutralino.app.exit()
     }
 })
