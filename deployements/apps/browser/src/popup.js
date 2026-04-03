f72ffdd8c286c4c2ce93fb8ff8b9762f45a08665
Louis-Charles Biron <louilresbiron6@gmail.com>
2026-04-03 10:17:10 -0400

    (See #116) UI progress

--- deployements/apps/browser/src/popup.js
+++ deployements/apps/browser/src/popup.js
@@ -12,7 +12,10 @@ chrome.management.getSelf(e=>document.getElementById("version").textContent="V"+
 // Creating the powder simulation
 const simulation = new Simulation(
     document.getElementById("simulationCanvas"),
-    readyCB,
+    (simulation)=>{
+        console.log("%cSIMULATION LOADED", "font-size:9.5px;color:#9c9c9c;")
+        simulation.updateBrushType(Simulation.BRUSH_TYPES.X3)
+    },
     {
         usesWebWorkers: false,
         aimedFPS: 60,
@@ -27,16 +30,44 @@ const simulation = new Simulation(
     }
 )
 
-// READY FUNCTION
-function readyCB(simulation) {
-    console.log("%cSIMULATION LOADED", "font-size:9.5px;color:#9c9c9c;")
-    if (!(simulation instanceof Simulation)) return
 
-    //simulation.updateSelectedMaterial(Simulation.MATERIALS.WATER)
-    //simulation.updateMapSize(231, 149)
-    //simulation.updateMapPixelSize(4)
-    simulation.updateBrushType(Simulation.BRUSH_TYPES.X3)
+// TABS SWITCHING
+const tabs = document.querySelectorAll(".tabs > span"), tabContents = document.querySelectorAll(".tabContent > div")
+
+function selectTab(e) {
+    const dataset = e.target.dataset
+
+    tabs.forEach(el=>el.dataset.selected = false)
+    tabContents.forEach(el=>el.dataset.displayed = false)
+
+    dataset.selected = true
+    ;[...tabContents].find(el=>el.dataset.content === dataset.content).dataset.displayed = true
 }
 
+tabs.forEach(el=>el.onclick=selectTab)
 
 
+// CSS VARIABLES
+const root = document.querySelector(":root").style
+root.setProperty("--tabsCount", tabs.length)
+
+
+// MATERIALS TAB
+const materialsList = document.getElementById("materialsList")
+Object.entries(Simulation.MATERIALS).forEach(([name, mat])=>{
+    const box = document.createElement("div")
+    box.textContent = name
+    box.onclick=()=>simulation.updateSelectedMaterial(mat)
+
+    materialsList.appendChild(box)
+})
+
+// BRUSHES TAB
+const brushesList = document.getElementById("brushesList")
+Object.entries(Simulation.BRUSH_TYPES).forEach(([name, brush])=>{
+    const box = document.createElement("div")
+    box.textContent = name
+    box.onclick=()=>simulation.updateBrushType(brush)
+
+    brushesList.appendChild(box)
+})
